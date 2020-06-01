d0a4e0e9f2e16d646f7f0c70a4fd55558ac257a1
patrykomiotek <patryk.omiotek@gmail.com>
2020-06-01 16:28:26 +0200

    initial commit

---
+++ src/components/Link/Link.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Link({ active }) {
+	return(
+		<a href="/">Name</a>
+	);
+}
+
+export default Link;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Link from '../Link';
+
+function Menu() {
+	return(
+  	<div className="menu">
+      <Link />
+      <Link />
+      <Link active />
+    </div>
+  );
+}
+
+export default Menu;
